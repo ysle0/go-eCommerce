a890c4e62c42de4afd88c737edfafa9c19afe435
yoonsang Lee <ysle@pm.me>
2025-04-24 01:45:27 +0900

    chore(db): resolve pkg name conflict

--- internal/transport/rest/init.go
+++ internal/transport/rest/init.go
@@ -2,12 +2,12 @@ package rest
 
 import (
 	"context"
-	"github.com/ysle0/go-starter/internal/db/mongo"
+	"github.com/ysle0/go-starter/internal/db/mongodb"
 	"github.com/ysle0/go-starter/internal/server"
 )
 
 func InitREST(ctx context.Context, s *server.Server) {
-	mongo.NewMongo(ctx, s.Cfg)
+	mongodb.NewMongoDb(ctx, s.Cfg)
 	useMiddlewares(s.Router, s.Cfg)
 	route(s.Router)
 }
